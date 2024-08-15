e8fd620873b63eecf569bd66b7f4dfbf5e307e82
mariosknl <mariosknl@outlook.com>
2024-08-15 10:15:49 +0300

    sync clerk users to supabase with webhooks & Edge functions

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,23 +2,70 @@ import { Stack } from "expo-router";
 import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
+import * as SecureStore from "expo-secure-store";
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+import { SupabaseProvider } from "@/context/SupabaseContext";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+	throw new Error(
+		"Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+	);
+}
+
+export interface TokenCache {
+	getToken: (key: string) => Promise<string | undefined | null>;
+	saveToken: (key: string, token: string) => Promise<void>;
+	clearToken?: (key: string) => void;
+}
+
+// cache the Clerk JWT token
+const tokenCache = {
+	async getToken(key: string) {
+		try {
+			const item = await SecureStore.getItemAsync(key);
+			if (item) {
+				console.log(`${key} was used 🔐 \n`);
+			} else {
+				console.log("No values stored under key: " + key);
+			}
+			return item;
+		} catch (error) {
+			console.error("SecureStore get item error: ", error);
+			await SecureStore.deleteItemAsync(key);
+			return null;
+		}
+	},
+	async saveToken(key: string, value: string) {
+		try {
+			return SecureStore.setItemAsync(key, value);
+		} catch (err) {
+			return;
+		}
+	},
+};
 
 const InitialLayout = () => {
 	return (
-		<Stack>
-			<Stack.Screen name="index" options={{ headerShown: false }} />
-		</Stack>
+		<SupabaseProvider>
+			<Stack>
+				<Stack.Screen name="index" options={{ headerShown: false }} />
+			</Stack>
+		</SupabaseProvider>
 	);
 };
 
 const RootLayoutNav = () => {
 	return (
-		<ActionSheetProvider>
-			<GestureHandlerRootView style={{ flex: 1 }}>
-				<StatusBar style="light" />
-				<InitialLayout />
-			</GestureHandlerRootView>
-		</ActionSheetProvider>
+		<ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+			<ActionSheetProvider>
+				<GestureHandlerRootView style={{ flex: 1 }}>
+					<StatusBar style="light" />
+					<InitialLayout />
+				</GestureHandlerRootView>
+			</ActionSheetProvider>
+		</ClerkProvider>
 	);
 };
 
--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,5 +1,7 @@
 import { Colors } from "@/constants/Colors";
+import { useWarmUpBrowser } from "@/hooks/useWarmUpBrowser";
 import { AuthStrategy, ModalType } from "@/types/enums";
+import { useOAuth, useSignIn, useSignUp } from "@clerk/clerk-expo";
 import { Ionicons } from "@expo/vector-icons";
 import { BottomSheetView } from "@gorhom/bottom-sheet";
 import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
@@ -31,10 +33,79 @@ interface AuthModalProps {
 }
 
 const AuthModal = ({ authType }: AuthModalProps) => {
+	useWarmUpBrowser();
+	const { signUp, setActive } = useSignUp();
+	const { signIn } = useSignIn();
+	const { startOAuthFlow: googleAuth } = useOAuth({
+		strategy: AuthStrategy.Google,
+	});
+	const { startOAuthFlow: microsoftAuth } = useOAuth({
+		strategy: AuthStrategy.Microsoft,
+	});
+	const { startOAuthFlow: slackAuth } = useOAuth({
+		strategy: AuthStrategy.Slack,
+	});
+	const { startOAuthFlow: appleAuth } = useOAuth({
+		strategy: AuthStrategy.Apple,
+	});
+
 	const onSelectedAuth = async (strategy: AuthStrategy) => {
-		console.log(strategy);
-		// Todo: Clerk Auth
+		if (!signIn || !signUp) return;
+
+		const selectedAuth = {
+			[AuthStrategy.Google]: googleAuth,
+			[AuthStrategy.Microsoft]: microsoftAuth,
+			[AuthStrategy.Slack]: slackAuth,
+			[AuthStrategy.Apple]: appleAuth,
+		}[strategy];
+
+		// if the user has an account in your application, but does not yet
+		// have an OAuth account connected to it, you can transfer the OAuth
+		// account to the existing user account.
+		const userExistsButNeedsToSignIn =
+			signUp.verifications.externalAccount.status === "transferable" &&
+			signUp.verifications.externalAccount.error?.code ===
+				"external_account_exists";
+
+		if (userExistsButNeedsToSignIn) {
+			const res = await signIn.create({ transfer: true });
+
+			if (res.status === "complete") {
+				setActive({
+					session: res.createdSessionId,
+				});
+			}
+		}
+
+		const userNeedsToBeCreated =
+			signIn.firstFactorVerification.status === "transferable";
+
+		if (userNeedsToBeCreated) {
+			const res = await signUp.create({
+				transfer: true,
+			});
+
+			if (res.status === "complete") {
+				setActive({
+					session: res.createdSessionId,
+				});
+			}
+		} else {
+			// If the user has an account in your application
+			// and has an OAuth account connected to it, you can sign them in.
+			try {
+				const { createdSessionId, setActive } = await selectedAuth();
+
+				if (createdSessionId) {
+					setActive!({ session: createdSessionId });
+					console.log("OAuth success standard");
+				}
+			} catch (err) {
+				console.error("OAuth error", err);
+			}
+		}
 	};
+
 	return (
 		<BottomSheetView style={styles.modalContainer}>
 			<TouchableOpacity style={styles.modalBtn}>
